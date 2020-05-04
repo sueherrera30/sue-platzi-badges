ac18537128f5952d74d9a76d3d5974f73b19e531
sue <sue@MacBook-Pro-de-sue.local>
2020-05-04 01:17:57 -0500

    delete notes

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,6 +1,7 @@
 //importamos react para poder usar JSx.
 import React from 'react';
 import './styles/Badge.css';
+// empaqueta archivos
 import confLogo from '../images/badge-header.svg';
 // import { Z_ASCII } from 'zlib';
 
@@ -12,9 +13,9 @@ class Badge extends React.Component {
     return (
       <div className="Badge">
       <div className="Badge__header">
+       {/* nos da un valor para que envalueesto seria una prop,crea un codigo un hash  */}
         <img src={confLogo} alt="Logo de la conferencia" />
       </div>
-
       <div className="Badge__section-name">
         <img
           className="Badge__avatar"
